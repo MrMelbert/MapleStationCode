8eb7bd7b8553b4eb087a83bd38f2f1381ead8d2c
Jolly-66 <freddieschauer@comcast.net>
2021-07-30 20:07:29 -0400

    bro what

--- tools/ci/run_server.sh
+++ tools/ci/run_server.sh
@@ -8,10 +8,6 @@ mkdir ci_test/config
 cp tools/ci/ci_config.txt ci_test/config/config.txt
 
 cd ci_test
-<<<<<<< HEAD
 DreamDaemon jollystation.dmb -close -trusted -verbose -params "log-directory=ci"
-=======
-DreamDaemon tgstation.dmb -close -trusted -verbose -params "log-directory=ci"
->>>>>>> 5d2a9dd843807bcad59d4a98be9fffb8992b8854
 cd ..
 cat ci_test/data/logs/ci/clean_run.lk
