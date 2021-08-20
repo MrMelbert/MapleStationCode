f07161bbbb225e0773e76e049d62ee3185cb9a23
MrMelbert <kmelbert4@gmail.com>
2021-08-20 02:01:43 -0500

    divider b gone

--- tgui/packages/tgui/interfaces/_AdvancedHereticPanel.js
+++ tgui/packages/tgui/interfaces/_AdvancedHereticPanel.js
@@ -41,7 +41,6 @@ export const _AdvancedHereticPanel = (props, context) => {
           )}
           <AdvancedTraitorPanelBackground />
         </Section>
-        <Divider />
         <Section
           title={`${ antag_type } Objectives`}
           buttons={(
