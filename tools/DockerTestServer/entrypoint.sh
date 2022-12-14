d0729c6cee056dd1eebc54da9d920903c59e7cf3
MrMelbert <kmelbert4@gmail.com>
2022-12-14 16:35:28 -0600

    CI moment p2

--- tools/DockerTestServer/entrypoint.sh
+++ tools/DockerTestServer/entrypoint.sh
@@ -15,12 +15,12 @@ PURPLE='\033[00;35m'
 
 # Override game config files
 echo -e "${PURPLE}[${YELLOW}---${PURPLE}]${RS} Copying default configuration files..."
-cp -frv /gamecfg_ro/* /tgstation/config
+cp -frv /gamecfg_ro/* /MapleStation/config
 echo -e "${PURPLE}[${GREEN}---${PURPLE}]${RS} ${GREEN}Copy complete!${RS}"
 
 # Override game config files
 echo -e "${PURPLE}[${YELLOW}---${PURPLE}]${RS} Overriding config files"
-cp -frv /gamecfg/* /tgstation/config
+cp -frv /gamecfg/* /MapleStation/config
 echo -e "${PURPLE}[${GREEN}---${PURPLE}]${RS} ${GREEN}Override complete!${RS}"
 
 # Override common game options using variables
@@ -31,9 +31,9 @@ DB_HOST="${DB_HOST:-db}"
 DB_USER="${DB_USER:-gamelord}"
 DB_PASS="${DB_PASS:-gamelord}"
 
-sed -i -r 's/(#|^)ADDRESS .*/ADDRESS '"$DB_HOST"'/' /tgstation/config/dbconfig.txt
-sed -i -r 's/(#|^)FEEDBACK_LOGIN .*/FEEDBACK_LOGIN '"$DB_USER"'/' /tgstation/config/dbconfig.txt
-sed -i -r 's/(#|^)FEEDBACK_PASSWORD .*/FEEDBACK_PASSWORD '"$DB_PASS"'/' /tgstation/config/dbconfig.txt
+sed -i -r 's/(#|^)ADDRESS .*/ADDRESS '"$DB_HOST"'/' /MapleStation/config/dbconfig.txt
+sed -i -r 's/(#|^)FEEDBACK_LOGIN .*/FEEDBACK_LOGIN '"$DB_USER"'/' /MapleStation/config/dbconfig.txt
+sed -i -r 's/(#|^)FEEDBACK_PASSWORD .*/FEEDBACK_PASSWORD '"$DB_PASS"'/' /MapleStation/config/dbconfig.txt
 
 function envvar_override () {
   ### FUNCTION for overriding options in a file using the exported environment variables
@@ -49,7 +49,7 @@ function envvar_override () {
     OPTION=`echo $p | cut -d "=" -f 1 | sed s/"$PREFIX"//`
     VALUE=`echo $p | cut -d "=" -f 2`
     # Comment out logic, comments line out if value is #
-    if [[ "${VALUE}" == "#" ]]; then 
+    if [[ "${VALUE}" == "#" ]]; then
       echo "Commenting out option \"$OPTION\" in $FILENAME"
       sed -i -r 's/^'"$OPTION"'.*/#&/' "$FILENAME"
     else
@@ -59,18 +59,18 @@ function envvar_override () {
   done
 }
 # Overriding game options
-envvar_override "TG_GAME_" "/tgstation/config/game_options.txt"
+envvar_override "TG_GAME_" "/MapleStation/config/game_options.txt"
 
 
 # Setting ranks
 export IFS=","
 if [[ ! -z "${CKEYRANKS}" ]]; then
   echo -e "${PURPLE}[${YELLOW}---${PURPLE}]${RS} Inserting ranks..."
-  echo "" > /tgstation/config/admins.txt
+  echo "" > /maplestation/config/admins.txt
   echo -e "${RED}admins.txt has been reset!${RS}"
   for RANK in $CKEYRANKS; do
     echo "$( echo $RANK | cut -d '=' -f 1 )is now$( echo $RANK | cut -d '=' -f 2 )"
-    printf "${RANK}\n" >> /tgstation/config/admins.txt
+    printf "${RANK}\n" >> /MapleStation/config/admins.txt
   done
   echo -e "${PURPLE}[${GREEN}---${PURPLE}]${RS} ${GREEN}CKEY Ranks set.${RS}"
 fi
@@ -81,5 +81,5 @@ echo -e "${PURPLE}[${GREEN}---${PURPLE}]${RS} ${GREEN}Override complete!${RS}"
 # Start DreamDaemon
 echo -e "${PURPLE}[${GREEN}---${PURPLE}]${RS} ${GREEN}Starting DreamDaemon ...${RS}"
 echo -e "${PURPLE}[${GREEN}---${PURPLE}]${RS} ${YELLOW}Enjoy! <3${RS}"
-cd /tgstation
-DreamDaemon tgstation.dmb -port 1337 -trusted -close -verbose
+cd /MapleStation
+DreamDaemon maplestation.dmb -port 1337 -trusted -close -verbose
