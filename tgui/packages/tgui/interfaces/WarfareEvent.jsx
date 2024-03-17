4472cb97a393cd42a1b80ba3a13471b8086c72b9
El Derp o Derp <elderpoderp@gmail.com>
2024-03-17 00:19:58 -0700

    I forgot everything I ever learned about HTML and JS.

---
+++ tgui/packages/tgui/interfaces/WarfareEvent.jsx
@@ -0,0 +1,27 @@
+import { useBackend } from '../backend';
+import { Button, LabeledList, Section } from '../components';
+import { Window } from '../layouts';
+
+export const WarfareEvent = (props, context) => {
+  const { act, data } = useBackend(context);
+  // Extract `health` and `color` variables from the `data` object.
+  const { health, color } = data;
+  return (
+    <Window resizable>
+      <Window.Content scrollable>
+        <Section title="Health status">
+          <LabeledList>
+            <LabeledList.Item label="Health">{health}</LabeledList.Item>
+            <LabeledList.Item label="Color">{color}</LabeledList.Item>
+            <LabeledList.Item label="Button">
+              <Button
+                content="Dispatch a 'test' action"
+                onClick={() => act('test')}
+              />
+            </LabeledList.Item>
+          </LabeledList>
+        </Section>
+      </Window.Content>
+    </Window>
+  );
+};
