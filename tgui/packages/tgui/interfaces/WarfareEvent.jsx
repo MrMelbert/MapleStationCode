029ac7245a113604fa4fa65bab572402f58abc2f
El Derp o Derp <Elderpoderp@gmail.com>
2024-03-28 13:24:19 -0700

    More TGUI work.

--- tgui/packages/tgui/interfaces/WarfareEvent.jsx
+++ tgui/packages/tgui/interfaces/WarfareEvent.jsx
@@ -1,21 +1,40 @@
 import { useBackend } from '../backend';
-import { Button, Section } from '../components';
+import { Button, Flex, Section } from '../components';
 import { Window } from '../layouts';
 
 export const WarfareEvent = (props, context) => {
   const { act, data } = useBackend(context);
   const { selectedShells, fireDirection } = data;
   return (
-    <Window title="Warfare Module" resizable theme="admin">
+    <Window title="Warfare Module" resizable theme="malfunction">
       <Window.Content scrollable>
         <Section title="Volley Configuration">
-          <Button
-            fluid
-            icon="bomb"
-            color="bad"
-            content="Fire Volley"
-            onClick={() => act('fireShells')}
-          />
+          <Flex
+            direction="row"
+            wrap="nowrap"
+            align="center"
+            justify="space-evenly"
+          >
+            <Flex.Item grow={1}>
+              <Button.Confirm
+                fluid
+                icon="bomb"
+                color="good"
+                children="Fire Volley"
+                confirmContent="Confirm Shell Volley?"
+                onClick={() => act('fireShells')}
+              />
+            </Flex.Item>
+            <Flex.Item grow={1}>
+              <Button
+                fluid
+                icon="arrows-rotate"
+                color="light-grey"
+                children="Change Firing Direction"
+                onClick={() => act('changeDirection')}
+              />
+            </Flex.Item>
+          </Flex>
           <Section title="Loaded Shells" />
         </Section>
       </Window.Content>
