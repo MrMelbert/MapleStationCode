9865da2c047d1501c2f218a43cb57a40d29f1c9d
MrMelbert <kmelbert4@gmail.com>
2025-03-03 02:44:27 -0600

    Lint

--- tgui/packages/tgui/interfaces/VotePanel.tsx
+++ tgui/packages/tgui/interfaces/VotePanel.tsx
@@ -244,7 +244,8 @@ const ChoicesPanel = (props) => {
                   buttons={
                     <Button
                       disabled={
-                        user.cannotVote || user.singleSelection === choice.name
+                        !!user.cannotVote ||
+                        user.singleSelection === choice.name
                       }
                       tooltip={user.cannotVote}
                       onClick={() => {
