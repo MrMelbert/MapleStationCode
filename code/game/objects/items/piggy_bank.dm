HolyTigerGod/Voidcrew

Adds a small cafeteria behind the right wing shutters of the museum. (#81465)
## About The Pull Request
I was thinking to contribute something to the new away mission map to
make it better. Mapping and all takes too much time for me, so I could
do little. Though it comes with its own unique gimmicks.

To reach the cafeteria, one has to complete a couple puzzles.
The first set is opened by inputing the correct PIN on the password
panel beside it. There're several clues to help you guess this fairly
easy puzzle, in the form of several number graffitis, a scrapped piece
of paper full of numbers, and a board filled with colored dots also
found just beside the panel.
The second one is opened by a keycard, and is generally lazier. To find
it, you'll need to do a bit of (toilet) searching.

As for the unique things this PR adds:
- A fire extinguisher... that actually contains welding fuel
- A (dirt-cheap) hotdog vending machine*
- A completely ornamental maneki-neko (that's the name of the
luck-bringing, paw-waving cat figurine)
- A piggy bank that carries money between rounds. It has a cap of 10k
credits worth of holochips, cash and coins, which is pretty high, but
I'm confident people will just destroy it for its contents the moment
they find it. His name is Pigston Swinelord VI.
- More, totally legit and not actually fake bombable walls :^)

*By the by, you can also find it during the national hotdog day.

Screenshots of the new location:
![museum
cafe](https://github.com/tgstation/tgstation/assets/42542238/1c0d93b7-90d5-4459-a48d-81430f0d3613)
![museum
restrooms](https://github.com/tgstation/tgstation/assets/42542238/5a9e049d-6acc-464b-998d-901e43154bae)


## Why It's Good For The Game
You know how most away missions are not that special at all? Yeah,
@mc-oofert set an example of a pretty decent one actually, if not a tad
small. I thought it could use a touch of another mind actually
contributing to it too, because it deserves it.

code/game/objects/items/piggy_bank.dm
@@ -1,0 +1,129 @@
+/**
+ * Piggy banks. They store your hard-earned money until you or someone destroys it.
+ * If the persistence id is set, money will be carried between rounds until broken.
+ */
+/obj/item/piggy_bank
+	name = "piggy bank"
+	desc = "A pig-shaped money container made of porkelain, oink. <i> Do not throw.</i>" //pun very intended.
+	icon = 'icons/obj/fluff/general.dmi'
+	icon_state = "piggy_bank"
+	max_integrity = 8
+	w_class = WEIGHT_CLASS_NORMAL
+	force = 12
+	throwforce = 15
+	throw_speed = 3
+	throw_range = 7
+	greyscale_config = /datum/greyscale_config/piggy_bank
+	///Some piggy banks are persistent, meaning they carry dosh between rounds.
+	var/persistence_id
+	///Callback to execute upon roundend to save the current amount of cash it has stored, IF persistent.
+	var/datum/callback/persistence_cb
+	///How much dosh can this piggy bank hold.
+	var/maximum_value = PAYCHECK_COMMAND * 20
+	///How much dosh this piggy bank spawns with.
+	var/initial_value = 0
+
+/obj/item/piggy_bank/Initialize(mapload)
+	if(!greyscale_colors)
+		greyscale_colors = pick(COLOR_PINK,
+			COLOR_LIGHT_ORANGE,
+			COLOR_GREEN_GRAY,
+			COLOR_PALE_BLUE_GRAY,
+			COLOR_DARK_MODERATE_LIME_GREEN,
+			COLOR_OFF_WHITE,
+		)
+
+	. = ..()
+
+	AddElement(/datum/element/can_shatter, shattering_sound = SFX_SHATTER, shatters_as_weapon = TRUE)
+	AddElement(/datum/element/beauty, 500)
+	if(!persistence_id)
+		if(initial_value)
+			new /obj/item/holochip(src, initial_value)
+		return
+
+	SSpersistence.load_piggy_bank(src)
+	persistence_cb = CALLBACK(src, PROC_REF(save_cash))
+	SSticker.OnRoundend(persistence_cb)
+
+	if(initial_value & initial_value + calculate_dosh_amount() <= maximum_value)
+		new /obj/item/holochip(src, initial_value)
+
+/obj/item/piggy_bank/proc/save_cash()
+	SSpersistence.save_piggy_bank(src)
+
+/obj/item/piggy_bank/Destroy()
+	if(persistence_cb)
+		LAZYREMOVE(SSticker.round_end_events, persistence_cb) //cleanup the callback.
+		persistence_cb = null
+	return ..()
+
+/obj/item/piggy_bank/deconstruct(disassembled = TRUE)
+	for(var/obj/item/thing as anything in contents)
+		thing.forceMove(loc)
+	//Smashing the piggy after the round is over doesn't count.
+	if(persistence_id && SSticker.current_state < GAME_STATE_FINISHED)
+		LAZYADD(SSpersistence.queued_broken_piggy_ids, persistence_id)
+	return ..()
+
+/obj/item/piggy_bank/attack_self(mob/user, modifiers)
+	. = ..()
+	if(DOING_INTERACTION_WITH_TARGET(user, src))
+		return
+	balloon_alert(user, "rattle rattle...")
+	if(!do_after(user, 0.5 SECONDS, src))
+		return
+	var/percentile = round(calculate_dosh_amount()/maximum_value * 100, 1)
+	if(percentile >= 10)
+		playsound(src, SFX_RATTLE, percentile * 0.5, FALSE, FALSE)
+	switch(percentile)
+		if(0)
+			balloon_alert(user, "it's empty")
+		if(1 to 9)
+			balloon_alert(user, "it's almost empty")
+		if(10 to 25)
+			balloon_alert(user, "it's some cash")
+		if(25 to 45)
+			balloon_alert(user, "it's plenty of cash")
+		if(45 to 70)
+			balloon_alert(user, "it feels almost full")
+		if(70 to 95)
+			balloon_alert(user, "it feels full")
+		if(95 to INFINITY)
+			balloon_alert(user, "brimming with cash")
+
+/obj/item/piggy_bank/attackby(obj/item/item, mob/user, params)
+	var/creds_value = item.get_item_credit_value()
+	if(isnull(creds_value))
+		return ..()
+
+	var/dosh_amount = calculate_dosh_amount()
+
+	if(dosh_amount >= maximum_value)
+		balloon_alert(user, "it's full!")
+	else if(dosh_amount + creds_value > maximum_value)
+		balloon_alert(user, "too much cash!")
+	else if(!user.transferItemToLoc(item, src))
+		balloon_alert(user, "stuck in your hands!")
+	else
+		balloon_alert(user, "inserted [creds_value] creds")
+	return TRUE
+
+///Returns the total amount of credits that its contents amount to.
+/obj/item/piggy_bank/proc/calculate_dosh_amount()
+	var/total_value = 0
+	for(var/obj/item/item in contents)
+		total_value += item.get_item_credit_value()
+	return total_value
+
+/obj/item/piggy_bank/museum
+	name = "Pigston Swinelord VI"
+	desc = "The museum's mascot piggy bank and favorite embezzler, known to carry donations between shifts without paying taxes. The space IRS hates him."
+	persistence_id = "museum_piggy"
+	greyscale_colors = COLOR_PINK
+	maximum_value = PAYCHECK_COMMAND * 100
+	initial_value = PAYCHECK_COMMAND * 4
+
+/obj/item/piggy_bank/museum/Initialize(mapload)
+	. = ..()
+	AddComponent(/datum/component/areabound) //do not steal.

MrMelbert/MapleStationCode

code/game/objects/items/piggy_bank.dm
@@ -1,0 +1,129 @@
+/**
+ * Piggy banks. They store your hard-earned money until you or someone destroys it.
+ * If the persistence id is set, money will be carried between rounds until broken.
+ */
+/obj/item/piggy_bank
+	name = "piggy bank"
+	desc = "A pig-shaped money container made of porkelain, oink. <i> Do not throw.</i>" //pun very intended.
+	icon = 'icons/obj/fluff/general.dmi'
+	icon_state = "piggy_bank"
+	max_integrity = 8
+	w_class = WEIGHT_CLASS_NORMAL
+	force = 12
+	throwforce = 15
+	throw_speed = 3
+	throw_range = 7
+	greyscale_config = /datum/greyscale_config/piggy_bank
+	///Some piggy banks are persistent, meaning they carry dosh between rounds.
+	var/persistence_id
+	///Callback to execute upon roundend to save the current amount of cash it has stored, IF persistent.
+	var/datum/callback/persistence_cb
+	///How much dosh can this piggy bank hold.
+	var/maximum_value = PAYCHECK_COMMAND * 20
+	///How much dosh this piggy bank spawns with.
+	var/initial_value = 0
+
+/obj/item/piggy_bank/Initialize(mapload)
+	if(!greyscale_colors)
+		greyscale_colors = pick(COLOR_PINK,
+			COLOR_LIGHT_ORANGE,
+			COLOR_GREEN_GRAY,
+			COLOR_PALE_BLUE_GRAY,
+			COLOR_DARK_MODERATE_LIME_GREEN,
+			COLOR_OFF_WHITE,
+		)
+
+	. = ..()
+
+	AddElement(/datum/element/can_shatter, shattering_sound = SFX_SHATTER, shatters_as_weapon = TRUE)
+	AddElement(/datum/element/beauty, 500)
+	if(!persistence_id)
+		if(initial_value)
+			new /obj/item/holochip(src, initial_value)
+		return
+
+	SSpersistence.load_piggy_bank(src)
+	persistence_cb = CALLBACK(src, PROC_REF(save_cash))
+	SSticker.OnRoundend(persistence_cb)
+
+	if(initial_value & initial_value + calculate_dosh_amount() <= maximum_value)
+		new /obj/item/holochip(src, initial_value)
+
+/obj/item/piggy_bank/proc/save_cash()
+	SSpersistence.save_piggy_bank(src)
+
+/obj/item/piggy_bank/Destroy()
+	if(persistence_cb)
+		LAZYREMOVE(SSticker.round_end_events, persistence_cb) //cleanup the callback.
+		persistence_cb = null
+	return ..()
+
+/obj/item/piggy_bank/atom_deconstruct(disassembled = TRUE)
+	for(var/obj/item/thing as anything in contents)
+		thing.forceMove(loc)
+	//Smashing the piggy after the round is over doesn't count.
+	if(persistence_id && SSticker.current_state < GAME_STATE_FINISHED)
+		LAZYADD(SSpersistence.queued_broken_piggy_ids, persistence_id)
+	return ..()
+
+/obj/item/piggy_bank/attack_self(mob/user, modifiers)
+	. = ..()
+	if(DOING_INTERACTION_WITH_TARGET(user, src))
+		return
+	balloon_alert(user, "rattle rattle...")
+	if(!do_after(user, 0.5 SECONDS, src))
+		return
+	var/percentile = round(calculate_dosh_amount()/maximum_value * 100, 1)
+	if(percentile >= 10)
+		playsound(src, SFX_RATTLE, percentile * 0.5, FALSE, FALSE)
+	switch(percentile)
+		if(0)
+			balloon_alert(user, "it's empty")
+		if(1 to 9)
+			balloon_alert(user, "it's almost empty")
+		if(10 to 25)
+			balloon_alert(user, "it's some cash")
+		if(25 to 45)
+			balloon_alert(user, "it's plenty of cash")
+		if(45 to 70)
+			balloon_alert(user, "it feels almost full")
+		if(70 to 95)
+			balloon_alert(user, "it feels full")
+		if(95 to INFINITY)
+			balloon_alert(user, "brimming with cash")
+
+/obj/item/piggy_bank/attackby(obj/item/item, mob/user, params)
+	var/creds_value = item.get_item_credit_value()
+	if(isnull(creds_value))
+		return ..()
+
+	var/dosh_amount = calculate_dosh_amount()
+
+	if(dosh_amount >= maximum_value)
+		balloon_alert(user, "it's full!")
+	else if(dosh_amount + creds_value > maximum_value)
+		balloon_alert(user, "too much cash!")
+	else if(!user.transferItemToLoc(item, src))
+		balloon_alert(user, "stuck in your hands!")
+	else
+		balloon_alert(user, "inserted [creds_value] creds")
+	return TRUE
+
+///Returns the total amount of credits that its contents amount to.
+/obj/item/piggy_bank/proc/calculate_dosh_amount()
+	var/total_value = 0
+	for(var/obj/item/item in contents)
+		total_value += item.get_item_credit_value()
+	return total_value
+
+/obj/item/piggy_bank/museum
+	name = "Pigston Swinelord VI"
+	desc = "The museum's mascot piggy bank and favorite embezzler, known to carry donations between shifts without paying taxes. The space IRS hates him."
+	persistence_id = "museum_piggy"
+	greyscale_colors = COLOR_PINK
+	maximum_value = PAYCHECK_COMMAND * 100
+	initial_value = PAYCHECK_COMMAND * 4
+
+/obj/item/piggy_bank/museum/Initialize(mapload)
+	. = ..()
+	AddComponent(/datum/component/areabound) //do not steal.