ce932f8f51994bbb3a64d8a2f029d4d3c010c477
siliconOpossum <siliconopossum@protonmail.com>
2025-05-11 17:59:30 -0500

    almost certainly this

--- tools/ci/copy_build_output.sh
+++ tools/ci/copy_build_output.sh
@@ -4,6 +4,6 @@ mkdir -p \
     $1/icons \
     $1/tgui/public \
 
-cp tgstation.dmb tgstation.rsc $1/
+cp maplestation.dmb maplestation.rsc $1/ # NON-MODULE CHANGE
 cp -r icons/* $1/icons/
 cp -r tgui/public/* $1/tgui/public/
