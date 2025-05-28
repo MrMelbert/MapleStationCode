HolyTigerGod/Voidcrew

Rocky DLXIII: Fitness to Athletics, Athletics skill influences Boxing against Boxers, Boxing overhaul, Evil Boxing (#82611)
## About The Pull Request

### Changes Fitness skill to Athletics skill; Removes the sprite growth
as the only meaningful reward

Basically just a rebranding so that it is more broad in its concept. It
isn't just about peak physique. It's about how you can apply it too.

Reaching Legendary rewards you with the Golden Gloves.

### Boxing Overhaul

I started it in this pr here
https://github.com/tgstation/tgstation/pull/80635

But this is a more significant overhaul of boxing from the ground up.
Namely, it now is majorly influenced by the Athletics skill, and also
more thoroughly aligns with conventional punching in its current state.
However, a major component of this is that boxing's breadth of mechanics
is only relevant when used against another boxer. Non-boxer targets are
only ever going to get smacked by a stamina punch, as it is currently
(and more or less the same values too). Additionally, boxers cannot
punch someone who is either unconscious or in stamina crit, so it can't
be used to maintain stamcrit.

- Boxing now has a One-Two Punch mechanic. Swapping between the left and
right mouse buttons to punch and maintaining this causes every second
punch to do more damage (to boxers). Breaking this chain by punching
with the same button twice will cause you to lose damage (against
boxers).

- Boxing now can have crits occur below 50 stamina damage (against
boxers). However, to knock someone out clean, the target must be
staggered from a previous crit first. (Or I guess be staggered at all,
maybe their shoelaces were tied or something). Crit probabilities are
determined by adding your Athletics probability skill bonuses to your
unarmed effectiveness, and then reduced by the targets own Athletics
skill bonuses. It is hard for two athletes to knock each other out in
one blow. It is trivial for a legendary b

code/modules/clothing/gloves/boxing.dm
@@ -6,6 +6,25 @@
 	equip_delay_other = 60
 	species_exception = list(/datum/species/golem) // now you too can be a golem boxing champion
 	clothing_traits = list(TRAIT_CHUNKYFINGERS)
+	/// Determines the version of boxing (or any martial art for that matter) that the boxing gloves gives
+	var/style_to_give = /datum/martial_art/boxing
+
+/obj/item/clothing/gloves/boxing/Initialize(mapload)
+	. = ..()
+	var/static/list/slapcraft_recipe_list = list(/datum/crafting_recipe/extendohand_l, /datum/crafting_recipe/extendohand_r)
+
+	AddComponent(
+		/datum/component/slapcrafting,\
+		slapcraft_recipes = slapcraft_recipe_list,\
+	)
+
+	AddComponent(/datum/component/martial_art_giver, style_to_give)
+
+/obj/item/clothing/gloves/boxing/evil
+	name = "evil boxing gloves"
+	desc = "These strange gloves radiate an unsually evil aura."
+	greyscale_colors = "#21211f"
+	style_to_give = /datum/martial_art/boxing/evil
 
 /obj/item/clothing/gloves/boxing/green
 	icon_state = "boxinggreen"
@@ -18,3 +37,16 @@
 /obj/item/clothing/gloves/boxing/yellow
 	icon_state = "boxingyellow"
 	greyscale_colors = "#d2a800"
+
+/obj/item/clothing/gloves/boxing/golden
+	name = "golden gloves"
+	desc = "The reigning champ of the station!"
+	icon_state = "boxinggold"
+	custom_materials = list(/datum/material/gold = SHEET_MATERIAL_AMOUNT*1)  //LITERALLY GOLD
+	material_flags = MATERIAL_EFFECTS | MATERIAL_AFFECT_STATISTICS
+	equip_delay_other = 120
+	resistance_flags = INDESTRUCTIBLE | LAVA_PROOF | FIRE_PROOF | UNACIDABLE
+
+/obj/item/clothing/gloves/boxing/golden/Initialize(mapload)
+	. = ..()
+	AddElement(/datum/element/skill_reward, /datum/skill/athletics)

MrMelbert/MapleStationCode

code/modules/clothing/gloves/boxing.dm
@@ -6,6 +6,25 @@
 	equip_delay_other = 60
 	species_exception = list(/datum/species/golem) // now you too can be a golem boxing champion
 	clothing_traits = list(TRAIT_CHUNKYFINGERS)
+	/// Determines the version of boxing (or any martial art for that matter) that the boxing gloves gives
+	var/style_to_give = /datum/martial_art/boxing
+
+/obj/item/clothing/gloves/boxing/Initialize(mapload)
+	. = ..()
+	var/static/list/slapcraft_recipe_list = list(/datum/crafting_recipe/extendohand_l, /datum/crafting_recipe/extendohand_r)
+
+	AddElement(
+		/datum/element/slapcrafting,\
+		slapcraft_recipes = slapcraft_recipe_list,\
+	)
+
+	AddComponent(/datum/component/martial_art_giver, style_to_give)
+
+/obj/item/clothing/gloves/boxing/evil
+	name = "evil boxing gloves"
+	desc = "These strange gloves radiate an unsually evil aura."
+	greyscale_colors = "#21211f"
+	style_to_give = /datum/martial_art/boxing/evil
 
 /obj/item/clothing/gloves/boxing/green
 	icon_state = "boxinggreen"
@@ -18,3 +37,16 @@
 /obj/item/clothing/gloves/boxing/yellow
 	icon_state = "boxingyellow"
 	greyscale_colors = "#d2a800"
+
+/obj/item/clothing/gloves/boxing/golden
+	name = "golden gloves"
+	desc = "The reigning champ of the station!"
+	icon_state = "boxinggold"
+	custom_materials = list(/datum/material/gold = SHEET_MATERIAL_AMOUNT*1)  //LITERALLY GOLD
+	material_flags = MATERIAL_EFFECTS | MATERIAL_AFFECT_STATISTICS
+	equip_delay_other = 120
+	resistance_flags = INDESTRUCTIBLE | LAVA_PROOF | FIRE_PROOF | UNACIDABLE
+
+/obj/item/clothing/gloves/boxing/golden/Initialize(mapload)
+	. = ..()
+	AddElement(/datum/element/skill_reward, /datum/skill/athletics)