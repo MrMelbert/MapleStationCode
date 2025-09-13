HolyTigerGod/Voidcrew

Update the Paramedic Jacket to match the uniform colors, gives Paramedics black workboots (Tactical Work Boots) to replace their pastel-blue shoes (#91902)

code/modules/vending/autodrobe.dm
@@ -154,7 +154,7 @@ GLOBAL_LIST_INIT(autodrobe_other_items, list(
 	/obj/item/clothing/head/wig/random = 6,
 	/obj/item/clothing/head/flatcap = 3,
 	/obj/item/clothing/suit/jacket/miljacket = 3,
-	/obj/item/clothing/shoes/jackboots = 3,
+	/obj/item/clothing/shoes/workboots/black = 3,
 	/obj/item/clothing/mask/fakemoustache = 3,
 	/obj/item/clothing/glasses/cold= 3,
 	/obj/item/clothing/glasses/heat= 3,

MrMelbert/MapleStationCode

code/modules/vending/autodrobe.dm
@@ -12,230 +12,230 @@
 			"name" = "Costumes",
 			"icon" = "mask",
 			"products" = list(
-				/obj/item/clothing/under/costume/gladiator = 1,
-				/obj/item/clothing/head/helmet/gladiator = 1,
-				/obj/item/clothing/suit/toggle/labcoat/mad = 1,
-				/obj/item/clothing/suit/bio_suit/plaguedoctorsuit = 1,
-				/obj/item/clothing/head/bio_hood/plague = 1,
-				/obj/item/clothing/mask/gas/plaguedoctor = 1,
-				/obj/item/clothing/under/rank/medical/doctor/nurse = 1,
-				/obj/item/clothing/suit/toggle/owlwings = 1,
-				/obj/item/clothing/under/costume/owl = 1,
-				/obj/item/clothing/mask/gas/owl_mask = 1,
-				/obj/item/clothing/suit/toggle/owlwings/griffinwings = 1,
-				/obj/item/clothing/under/costume/griffin = 1,
-				/obj/item/clothing/shoes/griffin = 1,
-				/obj/item/clothing/head/costume/griffin = 1,
-				/obj/item/clothing/under/costume/pirate = 1,
-				/obj/item/clothing/suit/costume/pirate = 1,
-				/obj/item/clothing/head/costume/pirate = 1,
-				/obj/item/clothing/head/costume/pirate/bandana = 1,
-				/obj/item/clothing/shoes/pirate = 1,
-				/obj/item/clothing/under/costume/soviet = 1,
-				/obj/item/clothing/head/costume/ushanka = 1,
-				/obj/item/clothing/accessory/vest_sheriff =1,
-				/obj/item/clothing/head/cowboy/brown =1,
-				/obj/item/clothing/head/cowboy/red =1,
-				/obj/item/clothing/head/cowboy/black =1,
-				/obj/item/clothing/head/costume/sombrero/green = 1,
-				/obj/item/clothing/suit/costume/poncho = 1,
-				/obj/item/clothing/suit/costume/poncho/green = 1,
-				/obj/item/clothing/suit/costume/poncho/red = 1,
-				/obj/item/clothing/suit/costume/snowman = 1,
-				/obj/item/clothing/head/costume/snowman = 1,
-				/obj/item/clothing/under/costume/referee = 1,
-				/obj/item/clothing/mask/whistle = 1,
+				/obj/item/clothing/under/costume/gladiator = 3,
+				/obj/item/clothing/head/helmet/gladiator = 3,
+				/obj/item/clothing/suit/toggle/labcoat/mad = 3,
+				/obj/item/clothing/suit/bio_suit/plaguedoctorsuit = 3,
+				/obj/item/clothing/head/bio_hood/plague = 3,
+				/obj/item/clothing/mask/gas/plaguedoctor = 3,
+				/obj/item/clothing/under/rank/medical/doctor/nurse = 3,
+				/obj/item/clothing/suit/toggle/owlwings = 3,
+				/obj/item/clothing/under/costume/owl = 3,
+				/obj/item/clothing/mask/gas/owl_mask = 3,
+				/obj/item/clothing/suit/toggle/owlwings/griffinwings = 3,
+				/obj/item/clothing/under/costume/griffin = 3,
+				/obj/item/clothing/shoes/griffin = 3,
+				/obj/item/clothing/head/costume/griffin = 3,
+				/obj/item/clothing/under/costume/pirate = 3,
+				/obj/item/clothing/suit/costume/pirate = 3,
+				/obj/item/clothing/head/costume/pirate = 3,
+				/obj/item/clothing/head/costume/pirate/bandana = 3,
+				/obj/item/clothing/shoes/pirate = 3,
+				/obj/item/clothing/under/costume/soviet = 3,
+				/obj/item/clothing/head/costume/ushanka = 3,
+				/obj/item/clothing/accessory/vest_sheriff = 3,
+				/obj/item/clothing/head/cowboy/brown = 3,
+				/obj/item/clothing/head/cowboy/red = 3,
+				/obj/item/clothing/head/cowboy/black = 3,
+				/obj/item/clothing/head/costume/sombrero/green = 3,
+				/obj/item/clothing/suit/costume/poncho = 3,
+				/obj/item/clothing/suit/costume/poncho/green = 3,
+				/obj/item/clothing/suit/costume/poncho/red = 3,
+				/obj/item/clothing/suit/costume/snowman = 3,
+				/obj/item/clothing/head/costume/snowman = 3,
+				/obj/item/clothing/under/costume/referee = 3,
+				/obj/item/clothing/mask/whistle = 3,
 				/obj/item/storage/backpack/henchmen = 5,
 				/obj/item/clothing/under/costume/henchmen = 5,
 				/obj/item/clothing/head/costume/jackbros = 5,
 				/obj/item/clothing/under/costume/jackbros = 5,
 				/obj/item/clothing/under/costume/deckers = 5,
-				/obj/item/clothing/under/costume/sailor = 1,
-				/obj/item/clothing/head/costume/delinquent = 1,
-				/obj/item/clothing/suit/costume/dracula = 1,
-				/obj/item/clothing/under/costume/draculass = 1,
-				/obj/item/clothing/suit/costume/gothcoat = 1,
-				/obj/item/clothing/glasses/eyepatch = 1,
-				/obj/item/clothing/glasses/eyepatch/medical = 1,
+				/obj/item/clothing/under/costume/sailor = 3,
+				/obj/item/clothing/head/costume/delinquent = 3,
+				/obj/item/clothing/suit/costume/dracula = 3,
+				/obj/item/clothing/under/costume/draculass = 3,
+				/obj/item/clothing/suit/costume/gothcoat = 3,
+				/obj/item/clothing/glasses/eyepatch = 3,
+				/obj/item/clothing/glasses/eyepatch/medical = 3,
 				/obj/item/clothing/under/costume/gi = 4,
-				/obj/item/clothing/head/soft/propeller_hat = 1,
-				/obj/item/clothing/neck/bowtie/rainbow = 1,
+				/obj/item/clothing/head/soft/propeller_hat = 3,
+				/obj/item/clothing/neck/bowtie/rainbow = 3,
 			),
 		),
 		list(
 			"name" = "Supernatural",
 			"icon" = "hand-sparkles",
 			"products" = list(
-				/obj/item/clothing/suit/costume/imperium_monk = 1,
-				/obj/item/clothing/suit/chaplainsuit/holidaypriest = 1,
-				/obj/item/clothing/suit/chaplainsuit/habit = 1,
-				/obj/item/clothing/head/chaplain/habit_veil = 1,
-				/obj/item/clothing/suit/chaplainsuit/whiterobe = 1,
-				/obj/item/clothing/head/wizard/marisa/fake = 1,
-				/obj/item/clothing/suit/wizrobe/marisa/fake = 1,
-				/obj/item/clothing/head/costume/witchwig = 1,
-				/obj/item/staff/broom = 1,
-				/obj/item/clothing/suit/wizrobe/fake = 1,
-				/obj/item/clothing/head/wizard/fake = 1,
+				/obj/item/clothing/suit/costume/imperium_monk = 3,
+				/obj/item/clothing/suit/chaplainsuit/holidaypriest = 3,
+				/obj/item/clothing/suit/chaplainsuit/habit = 3,
+				/obj/item/clothing/head/chaplain/habit_veil = 3,
+				/obj/item/clothing/suit/chaplainsuit/whiterobe = 3,
+				/obj/item/clothing/head/wizard/marisa/fake = 3,
+				/obj/item/clothing/suit/wizrobe/marisa/fake = 3,
+				/obj/item/clothing/head/costume/witchwig = 3,
+				/obj/item/staff/broom = 3,
+				/obj/item/clothing/suit/wizrobe/fake = 3,
+				/obj/item/clothing/head/wizard/fake = 3,
 				/obj/item/staff = 3,
-				/obj/item/clothing/head/costume/shrine_wig = 1,
-				/obj/item/clothing/suit/costume/shrine_maiden = 1,
-				/obj/item/gohei = 1,
+				/obj/item/clothing/head/costume/shrine_wig = 3,
+				/obj/item/clothing/suit/costume/shrine_maiden = 3,
+				/obj/item/gohei = 3,
 			),
 		),
 		list(
 			"name" = "Entertainers",
 			"icon" = "masks-theater",
 			"products" = list(
-				/obj/item/clothing/under/rank/civilian/clown/blue = 1,
-				/obj/item/clothing/under/rank/civilian/clown/green = 1,
-				/obj/item/clothing/under/rank/civilian/clown/yellow = 1,
-				/obj/item/clothing/under/rank/civilian/clown/orange = 1,
-				/obj/item/clothing/under/rank/civilian/clown/purple = 1,
-				/obj/item/clothing/mask/gas/sexyclown = 1,
-				/obj/item/clothing/under/rank/civilian/clown/sexy = 1,
+				/obj/item/clothing/under/rank/civilian/clown/blue = 3,
+				/obj/item/clothing/under/rank/civilian/clown/green = 3,
+				/obj/item/clothing/under/rank/civilian/clown/yellow = 3,
+				/obj/item/clothing/under/rank/civilian/clown/orange = 3,
+				/obj/item/clothing/under/rank/civilian/clown/purple = 3,
+				/obj/item/clothing/mask/gas/sexyclown = 3,
+				/obj/item/clothing/under/rank/civilian/clown/sexy = 3,
 				/obj/item/clothing/head/beret = 6,
-				/obj/item/clothing/mask/gas/sexymime = 1,
-				/obj/item/clothing/under/rank/civilian/mime/sexy = 1,
-				/obj/item/clothing/under/rank/civilian/mime/skirt = 1,
-				/obj/item/clothing/under/rank/civilian/clown/jester = 1,
-				/obj/item/clothing/head/costume/jester = 1,
-				/obj/item/clothing/shoes/jester_shoes = 1,
-				/obj/item/clothing/under/costume/villain = 1,
-				/obj/item/clothing/suit/costume/joker = 1,
-				/obj/item/clothing/under/costume/joker = 1,
-				/obj/item/clothing/shoes/singery = 1,
-				/obj/item/clothing/under/costume/singer/yellow = 1,
-				/obj/item/clothing/shoes/singerb = 1,
-				/obj/item/clothing/under/costume/singer/blue = 1,
-				/obj/item/clothing/head/costume/cueball = 1,
+				/obj/item/clothing/mask/gas/sexymime = 3,
+				/obj/item/clothing/under/rank/civilian/mime/sexy = 3,
+				/obj/item/clothing/under/rank/civilian/mime/skirt = 3,
+				/obj/item/clothing/under/rank/civilian/clown/jester = 3,
+				/obj/item/clothing/head/costume/jester = 3,
+				/obj/item/clothing/shoes/jester_shoes = 3,
+				/obj/item/clothing/under/costume/villain = 3,
+				/obj/item/clothing/suit/costume/joker = 3,
+				/obj/item/clothing/under/costume/joker = 3,
+				/obj/item/clothing/shoes/singery = 3,
+				/obj/item/clothing/under/costume/singer/yellow = 3,
+				/obj/item/clothing/shoes/singerb = 3,
+				/obj/item/clothing/under/costume/singer/blue = 3,
+				/obj/item/clothing/head/costume/cueball = 3,
 			),
 		),
 		list(
 			"name" = "Fancy",
 			"icon" = "user-tie",
 			"products" = list(
-				/obj/item/clothing/under/rank/captain/suit = 1,
-				/obj/item/clothing/under/rank/captain/suit/skirt = 1,
-				/obj/item/clothing/under/costume/schoolgirl = 1,
-				/obj/item/clothing/under/costume/schoolgirl/red = 1,
-				/obj/item/clothing/under/costume/schoolgirl/green = 1,
-				/obj/item/clothing/under/costume/schoolgirl/orange = 1,
-				/obj/item/clothing/under/dress/skirt = 1,
+				/obj/item/clothing/under/rank/captain/suit = 3,
+				/obj/item/clothing/under/rank/captain/suit/skirt = 3,
+				/obj/item/clothing/under/costume/schoolgirl = 3,
+				/obj/item/clothing/under/costume/schoolgirl/red = 3,
+				/obj/item/clothing/under/costume/schoolgirl/green = 3,
+				/obj/item/clothing/under/costume/schoolgirl/orange = 3,
+				/obj/item/clothing/under/dress/skirt = 3,
 				/obj/item/clothing/neck/tie = 3,
-				/obj/item/clothing/head/hats/tophat = 1,
-				/obj/item/clothing/under/costume/kilt = 1,
+				/obj/item/clothing/head/hats/tophat = 3,
+				/obj/item/clothing/under/costume/kilt = 3,
 				/obj/item/clothing/glasses/monocle =1,
-				/obj/item/clothing/head/hats/bowler = 1,
-				/obj/item/cane = 1,
-				/obj/item/clothing/under/dress/sundress = 1,
-				/obj/item/clothing/suit/costume/whitedress = 1,
-				/obj/item/clothing/suit/costume/changshan_red = 1,
-				/obj/item/clothing/suit/costume/changshan_blue = 1,
-				/obj/item/clothing/suit/costume/cheongsam_red = 1,
-				/obj/item/clothing/suit/costume/cheongsam_blue = 1,
+				/obj/item/clothing/head/hats/bowler = 3,
+				/obj/item/cane = 3,
+				/obj/item/clothing/under/dress/sundress = 3,
+				/obj/item/clothing/suit/costume/whitedress = 3,
+				/obj/item/clothing/suit/costume/changshan_red = 3,
+				/obj/item/clothing/suit/costume/changshan_blue = 3,
+				/obj/item/clothing/suit/costume/cheongsam_red = 3,
+				/obj/item/clothing/suit/costume/cheongsam_blue = 3,
 			),
 		),
 		list(
 			"name" = "Animals",
 			"icon" = "paw",
 			"products" = list(
-				/obj/item/clothing/head/costume/kitty = 1,
+				/obj/item/clothing/head/costume/kitty = 3,
 				/obj/item/clothing/head/costume/rabbitears =1,
-				/obj/item/clothing/suit/costume/chickensuit = 1,
-				/obj/item/clothing/head/costume/chicken = 1,
-				/obj/item/clothing/suit/hooded/carp_costume = 1,
-				/obj/item/clothing/suit/hooded/ian_costume = 1,
-				/obj/item/clothing/suit/hooded/bee_costume = 1,
-				/obj/item/clothing/mask/animal/small/bat = 1,
-				/obj/item/clothing/mask/animal/small/bee = 1,
-				/obj/item/clothing/mask/animal/small/bear = 1,
-				/obj/item/clothing/mask/animal/small/raven = 1,
-				/obj/item/clothing/mask/animal/small/jackal = 1,
-				/obj/item/clothing/mask/animal/small/fox = 1,
-				/obj/item/clothing/mask/animal/frog = 1,
-				/obj/item/clothing/mask/animal/small/rat = 1,
-				/obj/item/clothing/mask/animal/pig = 1,
-				/obj/item/clothing/mask/animal/cowmask = 1,
-				/obj/item/clothing/mask/animal/horsehead = 1,
+				/obj/item/clothing/suit/costume/chickensuit = 3,
+				/obj/item/clothing/head/costume/chicken = 3,
+				/obj/item/clothing/suit/hooded/carp_costume = 3,
+				/obj/item/clothing/suit/hooded/ian_costume = 3,
+				/obj/item/clothing/suit/hooded/bee_costume = 3,
+				/obj/item/clothing/mask/animal/small/bat = 3,
+				/obj/item/clothing/mask/animal/small/bee = 3,
+				/obj/item/clothing/mask/animal/small/bear = 3,
+				/obj/item/clothing/mask/animal/small/raven = 3,
+				/obj/item/clothing/mask/animal/small/jackal = 3,
+				/obj/item/clothing/mask/animal/small/fox = 3,
+				/obj/item/clothing/mask/animal/frog = 3,
+				/obj/item/clothing/mask/animal/small/rat = 3,
+				/obj/item/clothing/mask/animal/pig = 3,
+				/obj/item/clothing/mask/animal/cowmask = 3,
+				/obj/item/clothing/mask/animal/horsehead = 3,
 			),
 		),
 		list(
 			"name" = "Service",
 			"icon" = "kitchen-set",
 			"products" = list(
-				/obj/item/clothing/under/costume/buttondown/slacks/service = 1,
-				/obj/item/clothing/under/costume/buttondown/skirt/service = 1,
-				/obj/item/clothing/neck/bowtie = 2,
-				/obj/item/clothing/accessory/waistcoat = 1,
-				/obj/item/clothing/under/suit/waiter = 1,
-				/obj/item/clothing/suit/apron = 1,
-				/obj/item/clothing/suit/apron/overalls = 1,
-				/obj/item/clothing/head/costume/maidheadband = 1,
-				/obj/item/clothing/under/costume/maid = 1,
-				/obj/item/clothing/gloves/maid = 1,
-				/obj/item/clothing/neck/maid = 1,
-				/obj/item/clothing/under/rank/civilian/janitor/maid = 1,
-				/obj/item/clothing/accessory/maidapron = 1,
+				/obj/item/clothing/under/costume/buttondown/slacks/service = 3,
+				/obj/item/clothing/under/costume/buttondown/skirt/service = 3,
+				/obj/item/clothing/neck/bowtie = 6,
+				/obj/item/clothing/accessory/waistcoat = 3,
+				/obj/item/clothing/under/suit/waiter = 3,
+				/obj/item/clothing/suit/apron = 3,
+				/obj/item/clothing/suit/apron/overalls = 3,
+				/obj/item/clothing/head/costume/maidheadband = 3,
+				/obj/item/clothing/under/costume/maid = 3,
+				/obj/item/clothing/gloves/maid = 3,
+				/obj/item/clothing/neck/maid = 3,
+				/obj/item/clothing/under/rank/civilian/janitor/maid = 3,
+				/obj/item/clothing/accessory/maidapron = 3,
 			),
 		),
 		list(
 			"name" = "Other",
 			"icon" = "star",
 			"products" = list(
-				/obj/item/clothing/head/wig/random = 3,
-				/obj/item/clothing/head/flatcap = 1,
-				/obj/item/clothing/suit/jacket/miljacket = 1,
-				/obj/item/clothing/shoes/jackboots = 1,
-				/obj/item/clothing/mask/fakemoustache = 1,
-				/obj/item/clothing/glasses/cold=1,
-				/obj/item/clothing/glasses/heat=1,
-				/obj/item/clothing/mask/gas/cyborg = 1,
-				/obj/item/clothing/mask/joy = 1,
-				/obj/item/clothing/mask/gas/prop = 4,
+				/obj/item/clothing/head/wig/random = 6,
+				/obj/item/clothing/head/flatcap = 3,
+				/obj/item/clothing/suit/jacket/miljacket = 3,
+				/obj/item/clothing/shoes/workboots/black = 6, // NON-MODULE CHANGE
+				/obj/item/clothing/mask/fakemoustache = 3,
+				/obj/item/clothing/glasses/cold = 3,
+				/obj/item/clothing/glasses/heat = 3,
+				/obj/item/clothing/mask/gas/cyborg = 3,
+				/obj/item/clothing/mask/joy = 3,
+				/obj/item/clothing/mask/gas/prop = 6,
 				/obj/item/clothing/mask/gas/atmosprop = 3,
-				/obj/item/clothing/mask/animal/small/tribal = 1,
+				/obj/item/clothing/mask/animal/small/tribal = 3,
 			),
 		),
 	)
 
 	contraband = list(
-		/obj/item/clothing/glasses/blindfold = 1,
+		/obj/item/clothing/glasses/blindfold = 3,
 		/obj/item/clothing/glasses/sunglasses/gar = 2,
-		/obj/item/clothing/head/costume/powdered_wig = 1,
-		/obj/item/clothing/head/costume/tv_head = 1,
+		/obj/item/clothing/head/costume/lobsterhat = 3,
+		/obj/item/clothing/head/costume/powdered_wig = 3,
+		/obj/item/clothing/head/costume/tv_head = 3,
 		/obj/item/clothing/mask/muzzle = 2,
-		/obj/item/clothing/shoes/clown_shoes/meown_shoes = 1,
-		/obj/item/clothing/shoes/clown_shoes/moffers = 1,
-		/obj/item/clothing/shoes/ducky_shoes = 1,
-		/obj/item/clothing/suit/costume/judgerobe = 1,
-		/obj/item/clothing/head/costume/lobsterhat = 1,
-		/obj/item/clothing/under/costume/lobster = 1,
+		/obj/item/clothing/shoes/clown_shoes/meown_shoes = 3,
+		/obj/item/clothing/shoes/clown_shoes/moffers = 3,
+		/obj/item/clothing/shoes/ducky_shoes = 3,
+		/obj/item/clothing/shoes/jackboots = 3, // NON-MODULE CHANGE
+		/obj/item/clothing/suit/costume/judgerobe = 3,
+		/obj/item/clothing/under/costume/lobster = 3,
 		/obj/item/gun/magic/wand/nothing = 2,
 		/obj/item/skillchip/musical = 3,
-		/obj/item/storage/box/tape_wizard = 1,
+		/obj/item/storage/box/tape_wizard = 3,
 
 	)
 	premium = list(
-		/obj/item/clothing/suit/costume/pirate/captain = 2,
-		/obj/item/clothing/head/costume/pirate/captain = 2,
-		/obj/item/clothing/under/rank/civilian/clown/rainbow = 1,
-		/obj/item/clothing/head/helmet/roman/fake = 1,
-		/obj/item/clothing/head/helmet/roman/legionnaire/fake = 1,
-		/obj/item/clothing/under/costume/roman = 1,
-		/obj/item/clothing/shoes/roman = 1,
-		/obj/item/shield/roman/fake = 1,
-		/obj/item/clothing/suit/chaplainsuit/clownpriest = 1,
-		/obj/item/clothing/head/chaplain/clownmitre = 1,
-		/obj/item/skub = 1,
-		/obj/item/clothing/suit/hooded/mysticrobe = 1,
-		/obj/item/clothing/under/dress/wedding_dress = 1,
-		/obj/item/clothing/under/suit/tuxedo = 1,
-		/obj/item/clothing/head/costume/weddingveil = 1,
-		/obj/item/storage/belt/fannypack/cummerbund = 1,
-		/obj/item/clothing/suit/costume/drfreeze_coat = 1,
-		/obj/item/clothing/under/costume/drfreeze = 1,
-		/obj/item/clothing/head/costume/drfreezehat = 1,
+		/obj/item/clothing/suit/costume/pirate/captain = 6,
+		/obj/item/clothing/under/rank/civilian/clown/rainbow = 3,
+		/obj/item/clothing/head/helmet/roman/fake = 3,
+		/obj/item/clothing/head/helmet/roman/legionnaire/fake = 3,
+		/obj/item/clothing/under/costume/roman = 3,
+		/obj/item/clothing/shoes/roman = 3,
+		/obj/item/shield/roman/fake = 3,
+		/obj/item/clothing/suit/chaplainsuit/clownpriest = 3,
+		/obj/item/clothing/head/chaplain/clownmitre = 3,
+		/obj/item/skub = 3,
+		/obj/item/clothing/suit/hooded/mysticrobe = 3,
+		/obj/item/clothing/under/dress/wedding_dress = 3,
+		/obj/item/clothing/under/suit/tuxedo = 3,
+		/obj/item/clothing/head/costume/weddingveil = 3,
+		/obj/item/storage/belt/fannypack/cummerbund = 3,
+		/obj/item/clothing/suit/costume/drfreeze_coat = 3,
+		/obj/item/clothing/under/costume/drfreeze = 3,
+		/obj/item/clothing/head/costume/drfreezehat = 3,
 	)
 	refill_canister = /obj/item/vending_refill/autodrobe
 	default_price = PAYCHECK_CREW * 0.8 //Default of 40.