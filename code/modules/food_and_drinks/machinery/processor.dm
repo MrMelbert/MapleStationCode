HolyTigerGod/Voidcrew

Xenobio tubes, QOL, & visual tweaks (#90775)
## About The Pull Request


https://github.com/user-attachments/assets/1b129bac-459f-47ed-b88f-fa0d295aa690

Xenobio no longer has slimes "warping around", but rather all mobs will
be suctioned up and out of a tube system.
This is functionally identical; the visual aspect is PURELY visual and
as such you can deposit a slime that's still visually being suctioned,
which is intentional for the sake of an easier user experience.
Additionally, slime consoles have a little HUD on the side now that
displays the amount of monkeys & the amount of currently stored slimes
inside of it. Nice for if you don't want to have to guess your current
monkey count.

Slime processors suction a maximum amount of slimes depending on their
matter bin level. This means that at a tier 1 bin it'll be the same as
current, but with a tier 4 you can suck up 4 slimes at a time per
processing tick. Doesn't really do any functionally different stuff
besides lowering the time you need to wait before you can start the
processor.

Monkey Recyclers have purple stripes to visually distinguish them from
meat grinders.
## Why It's Good For The Game

I want a funny claw-machine-esque contraption for xenobio.
## Changelog
:cl: Wallem
add: Xenobio consoles now work off a tube system instead of sudden
teleportation
qol: Xenobio consoles have a new HUD to show monkey/slime amounts, and
slime processors can now suck up more than 1 slime at a time depending
on their matter bin
image: Added some stripes to the monkey recycler to make it a little
more obvious it's not a meat grinder
/:cl:

code/modules/food_and_drinks/machinery/processor.dm
@@ -224,22 +224,26 @@
 /obj/machinery/processor/slime/process()
 	if(processing)
 		return
-	var/mob/living/basic/slime/picked_slime
+	var/list/mob/living/basic/slime/picked_slimes
+	/// We pick up a number of slimes equal to the rating of the matter bin
+	var/slimes_picked = 0
 	for(var/mob/living/basic/slime/slime in range(1,src))
 		if(!CanReach(slime)) //don't take slimes behind glass panes or somesuch; also makes it ignore slimes inside the processor
 			continue
 		if(slime.stat)
-			picked_slime = slime
+			var/datum/food_processor_process/recipe = PROCESSOR_SELECT_RECIPE(slime)
+			if(!recipe)
+				continue
+			LAZYADD(picked_slimes, slime)
+			slimes_picked += 1
+		if(slimes_picked >= rating_amount)
 			break
-	if(!picked_slime)
+	if(!LAZYLEN(picked_slimes))
 		return
-	var/datum/food_processor_process/recipe = PROCESSOR_SELECT_RECIPE(picked_slime)
-	if (!recipe)
-		return
-
-	visible_message(span_notice("[picked_slime] is sucked into [src]."))
-	LAZYADD(processor_contents, picked_slime)
-	picked_slime.forceMove(src)
+	visible_message(span_notice("[jointext(picked_slimes, ", ")] [LAZYLEN(picked_slimes) > 1 ? "are" : "is"] sucked into [src]."))
+	for(var/mob/living/basic/slime/slime_to_add in picked_slimes)
+		LAZYADD(processor_contents, slime_to_add)
+		slime_to_add.forceMove(src)
 
 /obj/machinery/processor/slime/process_food(datum/food_processor_process/recipe, atom/movable/what)
 	var/mob/living/basic/slime/processed_slime = what

MrMelbert/MapleStationCode

code/modules/food_and_drinks/machinery/processor.dm
@@ -220,22 +220,26 @@
 /obj/machinery/processor/slime/process()
 	if(processing)
 		return
-	var/mob/living/simple_animal/slime/picked_slime
+	var/list/mob/living/simple_animal/slime/picked_slimes
+	/// We pick up a number of slimes equal to the rating of the matter bin
+	var/slimes_picked = 0
 	for(var/mob/living/simple_animal/slime/slime in range(1,src))
 		if(!CanReach(slime)) //don't take slimes behind glass panes or somesuch; also makes it ignore slimes inside the processor
 			continue
 		if(slime.stat)
-			picked_slime = slime
+			var/datum/food_processor_process/recipe = PROCESSOR_SELECT_RECIPE(slime)
+			if(!recipe)
+				continue
+			LAZYADD(picked_slimes, slime)
+			slimes_picked += 1
+		if(slimes_picked >= rating_amount)
 			break
-	if(!picked_slime)
+	if(!LAZYLEN(picked_slimes))
 		return
-	var/datum/food_processor_process/recipe = PROCESSOR_SELECT_RECIPE(picked_slime)
-	if (!recipe)
-		return
-
-	visible_message(span_notice("[picked_slime] is sucked into [src]."))
-	LAZYADD(processor_contents, picked_slime)
-	picked_slime.forceMove(src)
+	visible_message(span_notice("[jointext(picked_slimes, ", ")] [LAZYLEN(picked_slimes) > 1 ? "are" : "is"] sucked into [src]."))
+	for(var/mob/living/simple_animal/slime/slime_to_add in picked_slimes)
+		LAZYADD(processor_contents, slime_to_add)
+		slime_to_add.forceMove(src)
 
 /obj/machinery/processor/slime/process_food(datum/food_processor_process/recipe, atom/movable/what)
 	var/mob/living/simple_animal/slime/processed_slime = what