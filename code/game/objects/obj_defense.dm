HolyTigerGod/Voidcrew

Demolition mod is inverted vs "soft" structures like spider webs (#90464)

code/game/objects/obj_defense.dm
@@ -4,7 +4,7 @@
 	var/damage_taken = hit_by.throwforce
 	if(isitem(hit_by))
 		var/obj/item/as_item = hit_by
-		damage_taken *= as_item.demolition_mod
+		damage_taken *= as_item.get_demolition_modifier(src)
 	take_damage(damage_taken, BRUTE, MELEE, 1, get_dir(src, hit_by))
 
 /obj/ex_act(severity, target)
@@ -35,7 +35,7 @@
 	var/damage_sustained = 0
 	if(!QDELETED(src)) //Bullet on_hit effect might have already destroyed this object
 		damage_sustained = take_damage(
-			hitting_projectile.damage * hitting_projectile.demolition_mod,
+			hitting_projectile.damage * hitting_projectile.get_demolition_modifier(src),
 			hitting_projectile.damage_type,
 			hitting_projectile.armor_flag,
 			FALSE,

code/game/objects/objs.dm
@@ -25,6 +25,7 @@
 	var/bare_wound_bonus = 0
 
 	/// A multiplier to an object's force when used against a structure, vehicle, machine, or robot.
+	/// Use [/obj/proc/get_demolition_modifier] to get the value.
 	var/demolition_mod = 1
 
 	/// Cached custom fire overlay
@@ -74,7 +75,8 @@ GLOBAL_LIST_EMPTY(objects_by_id_tag)
 	if(!attacking_item.force)
 		return
 
-	var/total_force = (attacking_item.force * attacking_item.demolition_mod)
+	var/demo_mod = attacking_item.get_demolition_modifier(src)
+	var/total_force = (attacking_item.force * demo_mod)
 	var/damage = take_damage(total_force, attacking_item.damtype, MELEE, TRUE, get_dir(src, user), attacking_item.armour_penetration)
 
 	// Sanity in case one is null for some reason
@@ -88,11 +90,15 @@ GLOBAL_LIST_EMPTY(objects_by_id_tag)
 	if (picked_index && length(attacking_item.attack_verb_simple) >= picked_index)
 		message_verb_simple = attacking_item.attack_verb_simple[picked_index]
 
-	if(attacking_item.demolition_mod > 1 && prob(damage * 5))
-		message_verb_simple = "pulverise"
-		message_verb_continuous = "pulverises"
+	if(demo_mod > 1 && prob(damage * 5))
+		if(HAS_TRAIT(src, TRAIT_INVERTED_DEMOLITION))
+			message_verb_simple = "shred"
+			message_verb_continuous = "shreds"
+		else
+			message_verb_simple = "pulverise"
+			message_verb_continuous = "pulverises"
 
-	if(attacking_item.demolition_mod < 1)
+	if(demo_mod < 1)
 		message_verb_simple = "ineffectively " + message_verb_simple
 		message_verb_continuous = "ineffectively " + message_verb_continuous
 
@@ -335,3 +341,9 @@ GLOBAL_LIST_EMPTY(objects_by_id_tag)
 	throwforce /= strength_mod
 	var/list/armor_mods = material.get_armor_modifiers(1 / multiplier)
 	set_armor(get_armor().generate_new_with_multipliers(armor_mods))
+
+/// Returns modifier to how much damage this object does to a target considered vulnerable to "demolition" (other objects, robots, etc)
+/obj/proc/get_demolition_modifier(obj/target)
+	if(HAS_TRAIT(target, TRAIT_INVERTED_DEMOLITION))
+		return (1 / demolition_mod)
+	return demolition_mod

MrMelbert/MapleStationCode

code/game/objects/obj_defense.dm
@@ -1,9 +1,9 @@
 
-/obj/hitby(atom/movable/AM, skipcatch, hitpush, blocked, datum/thrownthing/throwingdatum)
+/obj/hitby(atom/movable/hit_by, skipcatch, hitpush, blocked, datum/thrownthing/throwingdatum)
 	. = ..()
 	if(QDELETED(src))
 		return
-	hit_by_damage(AM, throwingdatum)
+	hit_by_damage(hit_by, throwingdatum)
 
 /obj/proc/hit_by_damage(atom/movable/hitting_us, datum/thrownthing/throwingdatum)
 	var/base_dam = hitting_us.throwforce
@@ -16,6 +16,7 @@
 	if(isitem(hitting_us))
 		var/obj/item/hit_item = hitting_us
 		base_dam += (5 * max(0, hit_item.w_class - 2))
+		base_dam *= as_item.get_demolition_modifier(src)
 
 	// no armor penetration
 	take_damage(base_dam, BRUTE, MELEE, TRUE, get_dir(src, hitting_us), 0)
@@ -112,7 +113,7 @@
 	var/damage_sustained = 0
 	if(!QDELETED(src)) //Bullet on_hit effect might have already destroyed this object
 		damage_sustained = take_damage(
-			hitting_projectile.damage * hitting_projectile.demolition_mod,
+			hitting_projectile.damage * hitting_projectile.get_demolition_modifier(src),
 			hitting_projectile.damage_type,
 			hitting_projectile.armor_flag,
 			FALSE,

code/game/objects/objs.dm
@@ -24,7 +24,8 @@
 	/// If this attacks a human with no wound armor on the affected body part, add this to the wound mod. Some attacks may be significantly worse at wounding if there's even a slight layer of armor to absorb some of it vs bare flesh
 	var/bare_wound_bonus = 0
 
-	/// A multiplier to an objecet's force when used against a stucture, vechicle, machine, or robot.
+	/// A multiplier to an object's force when used against a structure, vehicle, machine, or robot.
+	/// Use [/obj/proc/get_demolition_modifier] to get the value.
 	var/demolition_mod = 1
 
 	// Access levels, used in modules\jobs\access.dm
@@ -82,15 +83,22 @@ GLOBAL_LIST_EMPTY(objects_by_id_tag)
 	if(!attacking_item.force)
 		return
 
-	var/total_force = (attacking_item.force * attacking_item.demolition_mod)
+	var/demo_mod = attacking_item.get_demolition_modifier(src)
+	var/total_force = (attacking_item.force * demo_mod)
+	var/damage = take_damage(total_force, attacking_item.damtype, MELEE, TRUE, get_dir(src, user), attacking_item.armour_penetration)
 
-	var/damage = take_damage(total_force, attacking_item.damtype, MELEE, 1, get_dir(src, user))
+	// Sanity in case one is null for some reason
+	var/picked_index = rand(max(length(attacking_item.attack_verb_simple), length(attacking_item.attack_verb_continuous)))
 
 	var/damage_verb = "hit"
 
-	if(attacking_item.demolition_mod > 1 && damage)
-		damage_verb = "pulverise"
-	if(attacking_item.demolition_mod < 1)
+	if(demo_mod > 1 && prob(damage * 5))
+		if(HAS_TRAIT(src, TRAIT_INVERTED_DEMOLITION))
+			damage_verb = "shred"
+		else
+			damage_verb = "pulverise"
+
+	if(demo_mod < 1)
 		damage_verb = "ineffectively pierce"
 
 	user.visible_message(span_danger("[user] [damage_verb][plural_s(damage_verb)] [src] with [attacking_item][damage ? "." : ", [no_damage_feedback]!"]"), \
@@ -378,3 +386,9 @@ GLOBAL_LIST_EMPTY(objects_by_id_tag)
 		pixel_z = anchored_tabletop_offset
 	else
 		pixel_z = initial(pixel_z)
+
+/// Returns modifier to how much damage this object does to a target considered vulnerable to "demolition" (other objects, robots, etc)
+/obj/proc/get_demolition_modifier(obj/target)
+	if(HAS_TRAIT(target, TRAIT_INVERTED_DEMOLITION))
+		return (1 / demolition_mod)
+	return demolition_mod