voidcrew/Voidcrew

Adds the 'mob_possessor' argument to all the instances of special() (#93410)
## About The Pull Request

a spooky special possession pr for october appears~~

(This is just some code cleanup)
Before, some procs had the `mob_possessor` arg, and some didn't. Which
made it a nightmare to add more positional args to it down the line.
This PR just ensures that all positional args are present in all the
instances of the proc.

## Why It's Good For The Game

Makes this proc chain slightly less of a mess to deal with.

## Changelog

N/A

code/modules/mob_spawn/corpses/nonhuman_corpses.dm
@@ -11,7 +11,7 @@
 		return
 	. = ..()
 
-/obj/effect/mob_spawn/corpse/ai/special(mob/living/silicon/ai/spawned/dead_ai)
+/obj/effect/mob_spawn/corpse/ai/special(mob/living/silicon/ai/spawned/dead_ai, mob/mob_possessor)
 	. = ..()
 	dead_ai.name = src.name
 	dead_ai.real_name = src.name
@@ -24,7 +24,7 @@
 	///the color of the slime you're spawning.
 	var/slime_species = /datum/slime_type/grey
 
-/obj/effect/mob_spawn/corpse/slime/special(mob/living/basic/slime/spawned_slime)
+/obj/effect/mob_spawn/corpse/slime/special(mob/living/basic/slime/spawned_slime, mob/mob_possessor)
 	. = ..()
 	spawned_slime.set_slime_type(slime_species)
 

MrMelbert/MapleStationCode

code/modules/mob_spawn/corpses/nonhuman_corpses.dm
@@ -11,7 +11,7 @@
 		return
 	. = ..()
 
-/obj/effect/mob_spawn/corpse/ai/special(mob/living/silicon/ai/spawned/dead_ai)
+/obj/effect/mob_spawn/corpse/ai/special(mob/living/silicon/ai/spawned/dead_ai, mob/mob_possessor)
 	. = ..()
 	dead_ai.name = src.name
 	dead_ai.real_name = src.name
@@ -24,7 +24,7 @@
 	///the color of the slime you're spawning.
 	var/slime_species = /datum/slime_type/grey
 
-/obj/effect/mob_spawn/corpse/slime/special(mob/living/simple_animal/slime/spawned_slime)
+/obj/effect/mob_spawn/corpse/slime/special(mob/living/simple_animal/slime/spawned_slime, mob/mob_possessor)
 	. = ..()
 	spawned_slime.set_slime_type(slime_species)
 